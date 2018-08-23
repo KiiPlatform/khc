a71d136c0246bf436fe8dd3fd9f4be6b2b68924f
satoshi <satoshi.kumano@kii.com>
2018-08-23 15:26:03 +0900

    Fixing socket callbacks.

--- tests/large-tests/secure_socket_impl.h
+++ tests/large-tests/secure_socket_impl.h
@@ -2,10 +2,18 @@
 #define __KII_CORE_SOCKET
 
 #include "kii_socket_callback.h"
+#include <openssl/ssl.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct _ssl_context
+{
+    SSL *ssl;
+    SSL_CTX *ssl_ctx;
+    int socket;
+} ssl_context_t;
 
 kii_sock_code_t
     s_connect_cb(void* socket_context, const char* host,
